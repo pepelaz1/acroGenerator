8ee781b68abbf567758ae8fe121fb6f826dccda8
Vladimir Baldetskiy <pepelaz@sibmail.com>
2015-04-19 18:13:46 +0600

    no message

---
+++ AcroGenerator/AcroGenerator/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AcroGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSourceFile = new System.Windows.Forms.TextBox();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.tbOutputFile = new System.Windows.Forms.TextBox();
+            this.btnSelectOutput = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output file";
+            // 
+            // tbSourceFile
+            // 
+            this.tbSourceFile.Location = new System.Drawing.Point(74, 21);
+            this.tbSourceFile.Name = "tbSourceFile";
+            this.tbSourceFile.Size = new System.Drawing.Size(417, 20);
+            this.tbSourceFile.TabIndex = 2;
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.Location = new System.Drawing.Point(491, 20);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(27, 22);
+            this.btnSelectSource.TabIndex = 3;
+            this.btnSelectSource.Text = "...";
+            this.btnSelectSource.UseVisualStyleBackColor = true;
+            this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
+            // 
+            // tbOutputFile
+            // 
+            this.tbOutputFile.Location = new System.Drawing.Point(74, 55);
+            this.tbOutputFile.Name = "tbOutputFile";
+            this.tbOutputFile.Size = new System.Drawing.Size(417, 20);
+            this.tbOutputFile.TabIndex = 4;
+            // 
+            // btnSelectOutput
+            // 
+            this.btnSelectOutput.Location = new System.Drawing.Point(491, 54);
+            this.btnSelectOutput.Name = "btnSelectOutput";
+            this.btnSelectOutput.Size = new System.Drawing.Size(27, 22);
+            this.btnSelectOutput.TabIndex = 5;
+            this.btnSelectOutput.Text = "...";
+            this.btnSelectOutput.UseVisualStyleBackColor = true;
+            this.btnSelectOutput.Click += new System.EventHandler(this.btnSelectOutput_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(211, 96);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(112, 22);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "Make acronymns";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // sfd
+            // 
+            this.sfd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 134);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnSelectOutput);
+            this.Controls.Add(this.tbOutputFile);
+            this.Controls.Add(this.btnSelectSource);
+            this.Controls.Add(this.tbSourceFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acronymn generator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSourceFile;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.TextBox tbOutputFile;
+        private System.Windows.Forms.Button btnSelectOutput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+    }
+}
+
---
+++ AcroGenerator/AcroGenerator/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AcroGenerator.Properties;
+using System.IO;
+
+namespace AcroGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectSource_Click(object sender, EventArgs e)
+        {
+            ofd.ShowDialog();
+            if (String.IsNullOrEmpty(ofd.FileName) == false)
+            {
+                tbSourceFile.Text = ofd.FileName;
+            }
+        }
+
+        private void btnSelectOutput_Click(object sender, EventArgs e)
+        {
+            sfd.ShowDialog();
+            if (String.IsNullOrEmpty(sfd.FileName) == false)
+            {
+                tbOutputFile.Text = sfd.FileName;
+            }
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbSourceFile.Text)
+                || String.IsNullOrEmpty(tbOutputFile.Text))
+            {
+                MessageBox.Show("You should enter source and output files","Warning");
+            }
+            else
+            {
+                Process();
+            }
+        }
+
+        private void Process()
+        {
+            try
+            {
+                using (TextReader tr = new StreamReader(tbSourceFile.Text))
+                {
+                    using (TextWriter tw = new StreamWriter(tbOutputFile.Text))
+                    {
+                        String srcline;
+                        String outline;
+                        for(;;)
+                        {
+                            srcline = tr.ReadLine();
+                            if (String.IsNullOrEmpty(srcline) == true)
+                                break;
+                            outline = "";
+                            String []parts = srcline.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                            foreach (String part in parts)
+                            {
+                                outline += part.ToUpper()[0].ToString();
+                            }
+                            tw.WriteLine(outline);
+                        }
+                    }
+
+                }
+                MessageBox.Show("Completed!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            sfd.InitialDirectory = Environment.CurrentDirectory;
+            tbSourceFile.Text = Settings.Default.Srcfile;
+            tbOutputFile.Text = Settings.Default.Outfile;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Srcfile = tbSourceFile.Text;
+            Settings.Default.Outfile = tbOutputFile.Text;
+            Settings.Default.Save();
+        }
+    }
+}
